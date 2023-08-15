f53b05ef8bffaadf8cbc8279f38f81e0d2976932
Carlos Sejas <carlos.sejas@ucb.edu.bo>
2023-08-14 23:41:10 -0400

    saludo por genero

--- saludar.js
+++ saludar.js
@@ -1,5 +1,14 @@
 function mostrarSaludo() {
     var nombre = document.getElementById("nombre").value;
-    var saludo = "Hola, " + nombre ;
+    var genero = document.getElementById("genero").value;
+    var saludo = "";
+
+    if (genero === "masculino") {
+        saludo = "Hola, Sr. " + nombre + ". ¡Bienvenido!";
+    } else if (genero === "femenino") {
+        saludo = "Hola, Sra. " + nombre + ". ¡Bienvenida!";
+    } else {
+        saludo = "Hola, " + nombre + ". ¡Bienvenidx!";
+    }
     document.getElementById("saludo").textContent = saludo;
 }
